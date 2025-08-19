54bdd6467c7f454971bde0eb542e0e1819cd55f5
mauricio23 <anmauri93@hotmail.com>
2025-08-18 21:08:06 -0500

    juegos mentales version 5
    se creo el archivo cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,9 @@
-import prompt
+from brain_games.cli import welcome_user 
 
 def main():
     print("Welcome to the Brain Games!")
 
-    name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
+    welcome_user()
 
 if __name__=="__main__":
     main() 
