8a2d2923461d62741de855b2caa568ff19e680e9
mauricio23 <anmauri93@hotmail.com>
2025-09-08 21:20:09 -0500

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,27 +1,36 @@
 import random
 from brain_games.cli import welcome_user 
 
-def par_impar():
+def par_impar(name):
     print('Answer "yes" if the number is even, otherwise answer "no"')
     ciclos = 3
-    for i in range(ciclos):
-        numero = random.randint(1,10)
+    for i in (ciclos):
+        numero = random.randint(2,10)
         print("Question: "+ str(numero))
         respuesta = input("Your answer: ").lower()
         correcta = ""
+
         if numero % 2 == 0:
             correcta = "yes"
         else: correcta = "no" 
-        
+        print(correcta)
+        print(respuesta)
         if respuesta == correcta:
             print("Correct!")
+        else: 
+            print(f"{respuesta} is wrong answer ;(. Correct answer was {correcta}.")
+            print(f"Let's try again, {name}!")
+            exit()
+
+        if i == 2:
+            print(f"Congratulations, {name}!")
 
 
 def main():
 
     print("Welcome to the Brain Games!")
-    welcome_user()
-    par_impar()
+    name = welcome_user()
+    par_impar(name)
 
 
 if __name__=="__main__":
