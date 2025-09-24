113ad338cc87b659433617a166eb8fdcc63f7da9
mauricio23 <anmauri93@hotmail.com>
2025-09-23 21:57:52 -0500

    juegos mentales version 14
    se termina el juego de maximo como un divisor

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,36 @@
+import random
+import math
+from brain_games.cli import welcome_user 
+
+def mcd(name):
+
+    print('Find the greatest common divisor of given numbers"')
+    ciclos = 3
+ 
+    for i in range(ciclos):
+        number1 = random.randint(2,20)
+        number2 = random.randint(2,20)
+        print("Question: "+ str(number1) + " " +str(number2))
+        respuesta = input("Your answer: ").lower()
+        resultado_correcto = math.gcd(number1, number2)
+        
+        if respuesta == str(resultado_correcto):
+             print("Correct!")
+        else: 
+            print(f"{respuesta} is wrong answer ;(. Correct answer was {str(resultado_correcto)}.")
+            print(f"Let's try again, {name}!")
+            exit()
+
+        if i == 2:
+            print(f"Congratulations,{name}!")
+
+
+def main():
+
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    mcd(name)
+
+
+if __name__=="__main__":
+    main() 
